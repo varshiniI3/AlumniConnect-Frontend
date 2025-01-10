35bb00f5bd59ab1b21c28f1d118dc732405f36f3
HEMANTH KUMAR SANGAM <109502503+Hemanthkumarsangam@users.noreply.github.com>
2025-01-10 11:01:06 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -88,7 +88,7 @@ function Home() {
         </span>
       </div>
       <div className="footer flex justify-around text-white" style={{background : '#003049'}}>
-        <a href="/" className='a' >Privacy policy</a>
+        <a href="/privactPolicy" className='a' >Privacy policy</a>
         <a href="/" className='a' >Terms & Conditions</a>
         <a href="/" ><FaSquareXTwitter className='text-4xl hover:-translate-y-2'/></a>
         <a href="/" ><FaSquareInstagram className='text-4xl hover:-translate-y-2'/></a>
