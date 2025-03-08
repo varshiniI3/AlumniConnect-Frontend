7165bb6cdf2977ca52925edca247e20f490d6c2e
varshiniI3 <109156904+varshiniI3@users.noreply.github.com>
2025-03-08 12:12:31 +0530

    Update Signup.jsx

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -4,10 +4,13 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 import './signup.css'
 import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
+import videoSrc from '../../assets/v1.mp4';
+import samplePerson from "../../assets/sample-person.jpg"
 
 function Signup (){
   const location = useLocation();
   const navigate = useNavigate();
+  const [imageUrl, setImageUrl] = useState(samplePerson);
   const [name, setName] = useState('')
   const [allowNameUpdt, setAllowNameUpdt] = useState(false)
   const [uname, setUname] = useState('')
@@ -17,7 +20,6 @@ function Signup (){
   const [file, setFile] = useState(null)
   const [designation, setDesignation] = useState('')
   const [email, setEmail] = useState('')
-  const [imageUrl, setImageUrl] = useState('')
   const [pass, setPass] = useState('')
   const [conPass, setConPass] = useState('')
   const [showPass, setShowPass] = useState(false)
@@ -54,10 +56,14 @@ function Signup (){
     }
   }, [location, navigate, email, token]);
 
+
   const handleFileChange = (e) => {
     const temp = e.target.files[0];
-    setFile(temp)
-  }
+    if (temp) {
+      setFile(temp);
+      setImageUrl(URL.createObjectURL(temp)); // Generate a preview
+    }
+  };
 
   const imageUpload = async() => {
     if(uname === ''){
@@ -121,47 +127,53 @@ function Signup (){
 
   return(
     <div className="signup flex gap-4 justify-center h-screen w-screen">
+      <div className="video-container">
+        <video autoPlay loop muted playsInline>
+          <source src={videoSrc} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
       <div className="formHolder flex gap-4 h-fit mt-8 w-11/12 relative">
         <div className="flex w-full">
           <div className='flex flex-col w-1/4'>
-            <label htmlFor="name">Name</label><hr />
-            <label htmlFor="email">Email</label><hr />
-            <label htmlFor="uname">Userame</label><hr/>
-            <label htmlFor="pass">Password</label><hr />
-            <label htmlFor="conpass">Confirm Password</label><hr />
-            <label htmlFor="rno">Roll number</label><hr/>
-            <label htmlFor="year">Passed out year</label><hr/>
-            <label htmlFor="branch">Branch</label><hr/>
+            <label htmlFor="name">Name</label>
+            <label htmlFor="email">Email</label>
+            <label htmlFor="uname">Userame</label>
+            <label htmlFor="pass">Password</label>
+            <label htmlFor="conpass">Confirm Password</label>
+            <label htmlFor="rno">Roll number</label>
+            <label htmlFor="year">Passed out year</label>
+            <label htmlFor="branch">Branch</label>
             <label htmlFor="role">Role</label>
-            {role && role === 'alumni' && <><hr/><label htmlFor="comp">Company</label><hr/></>}
+            {role && role === 'alumni' && <><label htmlFor="comp">Company</label></>}
             {role && role === 'alumni' && <label htmlFor="desg">Designation</label>}
           </div>
           <form className='flex flex-col w-3/5' onSubmit={handleSubmit}>
-            <input type="text" id='name' value={name} onChange={(e) => {setName(e.target.value)}} required/><hr />
-            <input type="email" id='email' value={email} disabled/><hr />
-            <input type="text" id='uname' value={uname} onChange={(e) => {setUname(e.target.value)}} required disabled={allowNameUpdt} /><hr/>
+            <input type="text" id='name' value={name} onChange={(e) => {setName(e.target.value)}} required/>
+            <input type="email" id='email' value={email} onChange={(e) => {setEmail(e.target.value)}} required/>
+            <input type="text" id='uname' value={uname} onChange={(e) => {setUname(e.target.value)}} required disabled={allowNameUpdt} />
             <div className="relative">
               <input type={showPass ? 'text' : 'password'} placeholder='Atleast 8 characters' value={pass} id="pass" onChange={(e) => {setPass(e.target.value)}} style={{width: '98%'}} required/>
               <span className="absolute right-3 top-2" onClick={() => {setShowPass(!showPass)}} >{showPass ? <IoMdEyeOff  /> : <IoMdEye  />}</span>
-            </div><hr />
+            </div>
             <div className="relative">
               <input type={showConPass ? 'text' : 'password'} placeholder='Atleast 8 characters' value={conPass} id="conpass" onChange={(e) => {setConPass(e.target.value)}} style={{width: '98%'}} required/>
               <span className="absolute right-3 top-2" onClick={() => {setShowConPass(!showConPass)}} >{showConPass ? <IoMdEyeOff  /> : <IoMdEye  />}</span>
-            </div><hr />
-            <input type="text" id='rno' value={regId} onChange={(e) => {setRegId(e.target.value)}} required/><hr/>
-            <input type="number" id='year' value={yop} onChange={(e) => {setYop(e.target.value)}} required/><hr/>
-            <input type="text" id='branch' value={branch} onChange={(e) => {setBranch(e.target.value)}} required/><hr/>
+            </div>
+            <input type="text" id='rno' value={regId} onChange={(e) => {setRegId(e.target.value)}} required/>
+            <input type="number" id='year' value={yop} onChange={(e) => {setYop(e.target.value)}} required/>
+            <input type="text" id='branch' value={branch} onChange={(e) => {setBranch(e.target.value)}} required/>
             <select id="role" value={role} onChange={(e) => {setRole(e.target.value)}}>
               <option value="student">student</option>
               <option value="alumni">alumni</option>
             </select>
-            {role && role === 'alumni' && <><hr/><input type="text" id='comp' value={company} onChange={e => setCompany(e.target.value)} required placeholder='Avoid acronyms'/><hr/></>}
+            {role && role === 'alumni' && <><input type="text" id='comp' value={company} onChange={e => setCompany(e.target.value)} required placeholder='Avoid acronyms'/></>}
             {role && role === 'alumni' && <input type="text" id='desg' value={designation} onChange={e => setDesignation(e.target.value)} required />}
             <center><button type="submit">Submit</button></center>
           </form>
         </div>
-        <div className='w-fit items-center flex flex-col gap-3'>
-          <img src={imageUrl} alt="porfileImage" className='rounded-full w-60 h-60'/>
+        <div className='profile w-fit items-center flex flex-col gap-5 '>
+          <img src={imageUrl} alt="" className='rounded-full w-60 h-60 border-2 border-red-700  rounded-full p4'/>
           <input type="file" name="" accept='image/*' id="imgFile" onChange={handleFileChange} style={{opacity: isUpdtProf ? '1' : '0'}} className='m-0 p-0 text-sm w-fit'/>
           <center>{isUpdtProf ? <button onClick={imageUpload}>Save</button> 
           : <button onClick={() => {setIsUpdtProf(!isUpdtProf)}}>Edit</button>}</center>
